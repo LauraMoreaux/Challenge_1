c6067d521ba4cb8a0215d662a6a7bab112e3484f
LauraMoreaux <lauramoreauxcueto@gmail.com>
2021-03-20 18:50:30 +0100

    Add filters style

--- src/components/StateFilters.js
+++ src/components/StateFilters.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 
 function StateFilters(props) {
+
+  const pressed = props.isPressed;
+
     return (
         <button
           type="button"
-          className="js-filter btn"
-          aria-pressed={props.isPressed}
+          className={`js-filter btn ${pressed ? "underlined" : ""}`}
+          aria-pressed={props.isPressed} 
           onClick={() => props.setFilter(props.name)}
         >
           <span className="">{props.name}</span>
